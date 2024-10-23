c13272927f64518870a9a4cc0ecbe1eb9e724053
Wojtek Mach <wojtekmach@users.noreply.github.com>
2024-10-23 23:08:45 +0200

    Initial implementation (#1)

---
+++ scripts/update_builds.bash
@@ -0,0 +1,56 @@
+#!/bin/bash
+set -euo pipefail
+
+main() {
+  git config user.name "github-actions[bot]"
+  git config user.email "github-actions[bot]@users.noreply.github.com"
+  if [[ "${OTP_REF_NAME}" == master ]] || [[ "${OTP_REF_NAME}" == maint* ]]; then
+    ref_name="${OTP_REF_NAME}-latest"
+  else
+    ref_name="${OTP_REF_NAME}"
+  fi
+  url="https://github.com/${GITHUB_REPOSITORY}/releases/download/${ref_name}/${TGZ}"
+  echo downloading $url
+  curl -fsSLO "${url}"
+
+  max_retries=5
+  attempt=0
+  while [ $attempt -lt $max_retries ]; do
+    if push; then
+      break
+    fi
+    attempt=$((attempt + 1))
+    echo "Retry $attempt/$max_retries failed. Retrying in 10 seconds..."
+    sleep 10
+  done
+
+  if [ $attempt -eq $max_retries ]; then
+    echo "Reached maximum retries ($max_retries). Exiting."
+    exit 1
+  fi
+}
+
+push() {
+  local target_branch=main
+
+  git checkout $target_branch
+  git reset --hard origin/$target_branch
+  git pull origin $target_branch
+  build_sha256=$(shasum -a 256 $TGZ | cut -d ' ' -f 1)
+  date=$(date -u '+%Y-%m-%dT%H:%M:%SZ')
+  mkdir -p builds/
+  touch "${BUILDS_CSV}"
+  sed -i.bak "/^${OTP_REF_NAME},/d" "${BUILDS_CSV}"
+  rm "${BUILDS_CSV}.bak"
+  echo -ne "${OTP_REF_NAME},${OTP_REF},${date},${build_sha256},openssl-${OPENSSL_VERSION},wxwidgets-${WXWIDGETS_VERSION}\n$(cat ${BUILDS_CSV})" > "${BUILDS_CSV}"
+  sort --reverse --unique -k1,1 -o "${BUILDS_CSV}" "${BUILDS_CSV}"
+  git add builds/
+  GIT_AUTHOR_NAME="${GITHUB_ACTOR}" \
+  GIT_AUTHOR_EMAIL="${GITHUB_ACTOR}@users.noreply.github.com" \
+  GIT_COMMITTER_NAME="github-actions[bot]" \
+  GIT_COMMITTER_EMAIL="github-actions[bot]@users.noreply.github.com" \
+    git commit -m "${BUILDS_CSV}: Add ${OTP_REF_NAME}"
+  git push origin $target_branch
+}
+
+main $@
---
+++ scripts/upload.bash
@@ -0,0 +1,106 @@
+#!/bin/bash
+set -euo pipefail
+
+usage() {
+  cat<<EOF
+ref_name=OTP-27.1.2; \
+  OTP_REF_NAME=$ref_name \
+  OPENSSL_VERSION=3.1.6 \
+  WXWIDGETS_VERSION=3.2.6 \
+  ./scripts/upload.bash
+EOF
+}
+
+main() {
+  : "${GITHUB_REPOSITORY:=erlef/otp_builds}"
+  : "${GITHUB_REF:=main}"
+  : "${BUILD_DIR:=$PWD/tmp/otp_builds}"
+  : "${OTP_TGZ:=$BUILD_DIR/otp.tar.gz}"
+  : "${ATTESTATION:=}"
+
+  if [ -z "${OTP_REF+x}" ]; then
+    OTP_REF=$(gh api repos/erlang/otp/commits/$OTP_REF_NAME --jq .sha)
+  fi
+
+  if [ "${OTP_REF_NAME}" = master ] || echo "${OTP_REF_NAME}" | grep -q "^maint"; then
+    ref_name="${OTP_REF_NAME}-latest"
+    notes="Automated build for https://github.com/erlang/otp/commit/${OTP_REF}."
+  else
+    ref_name="${OTP_REF_NAME}"
+    notes="Automated build for https://github.com/erlang/otp/releases/tag/${OTP_REF_NAME}."
+  fi
+
+  if ! gh release view $ref_name; then
+    latest="--latest=false"
+
+    if ! echo "$ref_name" | grep -qE 'rc|maint|master'; then
+      if [ -f builds/aarch64-apple-darwin.csv ]; then
+        latest_version=`cat builds/aarch64-apple-darwin.csv | cut -d"," -f1 | grep OTP- | sed 's/OTP-//' | sort --reverse -V | head -1`
+        version=$(echo "$ref_name" | sed 's/OTP-//')
+
+        if [ $(printf "%s\n%s" "$latest_version" "$version" | sort --reverse -V | head -1) != "$latest_version" ]; then
+          latest="--latest"
+        fi
+      fi
+    fi
+
+    # Initial commit
+    target=b5893a3c3a8d0ab54be5d04de450b24d9e5aa149
+
+    gh release create \
+      --repo "${GITHUB_REPOSITORY}" \
+      --title "$ref_name" \
+      --notes "$notes" \
+      --target "$target" \
+      $latest \
+      $ref_name
+  fi
+
+  arch=$(uname -m)
+  case "$arch" in
+    x86_64)
+      target="x86_64-apple-darwin"
+      legacy_target="macos-amd64"
+      ;;
+    arm64)
+      target="aarch64-apple-darwin"
+      legacy_target="macos-arm64"
+      ;;
+    *)
+      echo "Unknown architecture: $arch"
+      exit 1
+      ;;
+  esac
+
+  mkdir -p /tmp/otp_builds
+  tgz="/tmp/otp_builds/${OTP_REF_NAME}-${target}.tar.gz"
+  cp $OTP_TGZ $tgz
+  legacy_tgz="/tmp/otp_builds/${OTP_REF_NAME}-${legacy_target}.tar.gz"
+  cp $OTP_TGZ $legacy_tgz
+
+  gh release upload \
+    --repo "${GITHUB_REPOSITORY}" \
+    --clobber \
+    $ref_name \
+    $tgz $legacy_tgz
+
+  if [ -n "${ATTESTATION}" ]; then
+    cp ${ATTESTATION} $tgz.sigstore
+    gh release upload \
+      --repo "${GITHUB_REPOSITORY}" \
+      --clobber \
+      $ref_name \
+      $tgz.sigstore
+  fi
+
+  gh workflow run update_builds.yaml \
+    --repo "${GITHUB_REPOSITORY}" \
+    --ref "${GITHUB_REF}" \
+    --field otp-ref-name="${OTP_REF_NAME}" \
+    --field otp-ref="${OTP_REF}" \
+    --field openssl-version="${OPENSSL_VERSION}" \
+    --field wxwidgets-version="${WXWIDGETS_VERSION}" \
+    --field target="${target}"
+}
+
+main $@
